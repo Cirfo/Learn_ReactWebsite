67f953c0e7ac4a8d2cc87e3f56ba7c3d3c72b636
artrooms <80927167+artrooms@users.noreply.github.com>
2025-03-14 17:34:28 +0100

    bootstrap
    index.css not useful now, and app content not useful but the file could be useful in the future

---
+++ cirfo-website/src/components/App.tsx
@@ -0,0 +1,7 @@
+import Message from "./Message";
+
+function App() {
+  return <div><Message></Message></div>
+}
+
+export default App; // export means that we can use the component somewhere else
---
+++ cirfo-website/src/components/Message.tsx
@@ -0,0 +1,8 @@
+// older project would use js based components, I'll use function based components
+
+function Message(){
+    const title = 'Hello World!... by Cirfo'
+    return <h1>{title}</h1>; // JavaScript XML (JSX) -> JS after compilation!
+}
+
+export default Message;
